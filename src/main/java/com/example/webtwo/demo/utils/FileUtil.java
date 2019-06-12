7721a2126053943827581deca573ad2ad0a8fc81
Narnia <software_good@163.com>
2019-06-12 10:31:54 +0800

    文件下载、输出到浏览器下载

--- src/main/java/com/example/webtwo/demo/utils/FileUtil.java
+++ src/main/java/com/example/webtwo/demo/utils/FileUtil.java
@@ -1,16 +1,17 @@
 
 package com.example.webtwo.demo.utils;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -23,20 +24,22 @@ public class FileUtil {
 
     /**
      * 获取连接属性
+     * 在JDK的java.net包中已经提供了访问HTTP协议的基本功能的类：HttpURLConnection
+     * HttpURLConnection是Java的标准类，它继承自URLConnection，可用于向指定网站发送GET请求、POST请求
      * @param urlPath 路径
      * @return
      */
-    public static HttpURLConnection getConnectProperties(String urlPath){
+    public static HttpURLConnection getConnectProperties(String urlPath) {
         try {
             URL url = new URL(urlPath);
-            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setDoOutput(true);
             conn.setDoInput(true);
             conn.setUseCaches(false);
             conn.setRequestMethod("POST");
             conn.setConnectTimeout(6000);
-            conn.setRequestProperty("ContentType","text/plain");
-            conn.setRequestProperty("CHARSET","UTF-8");
+            conn.setRequestProperty("ContentType", "text/plain");
+            conn.setRequestProperty("CHARSET", "UTF-8");
             return conn;
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -45,6 +48,7 @@ public class FileUtil {
         }
         return null;
     }
+
     /**
      * 将所有文件压缩到输出流
      *
@@ -139,6 +143,104 @@ public class FileUtil {
         } catch (Exception e) {
             e.printStackTrace();
             logger.error("发生异常，压缩文件失败", e);
+        }
+    }
+
+    /**
+     * 从输入流中读取字节数组
+     *
+     * @param inputStream
+     * @return
+     */
+    public static byte[] getBytesFromInputStream(InputStream inputStream) {
+        //判断有效性
+        if (inputStream == null) {
+            return null;
+        }
+        try {
+            //设置返回数组
+            List<Integer> rs = new ArrayList<Integer>();
+            int b = -1;
+            while ((b = inputStream.read()) != -1) {
+                rs.add(b);
+            }
+            byte[] rtn = new byte[rs.size()];
+            for (int i = 0; i < rtn.length; i++) {
+                rtn[i] = rs.get(i).byteValue();
+            }
+            return rtn;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 按照日期获取文件
+     * @param filePath 文件路径
+     * @return
+     */
+    public static File[] getAppZipFile(String filePath) {
+        File[] files = new File[]{};
+        //根目录
+        File root = new File(filePath);
+        if (root.exists() && root.isDirectory() && root.canRead()) {
+            List<File> list = new ArrayList<File>();
+            File[] temp = root.listFiles();
+            //遍历文件来获取符合条件的文件
+            for (int i = 0; i < temp.length; i++) {
+                //添加文件
+                list.add(temp[i]);
+            }
+            files = new File[list.size()];
+            list.toArray(files);
+        }
+        return files;
+    }
+
+    /**
+     * 临时文件保存路径（路径下文件用完后删除）
+     * @param tempRootPath 路径
+     * @param fileSuffix 后缀
+     * @return
+     */
+    public static String getTempPath(String tempRootPath,String fileSuffix){
+        String tempFile = tempRootPath + File.separator + System.currentTimeMillis();
+        return tempFile + fileSuffix;
+    }
+
+    /**
+     * 获取二进制文件
+     * @param filePath 文件路径
+     * @return
+     * @throws IOException
+     */
+    public static byte[] readBinaryDataFromFile(String filePath) throws IOException{
+        File file = new File(filePath);
+        return FileUtils.readFileToByteArray(file);
+    }
+
+    /**
+     * 删除文件
+     * @param fileName 文件路径
+     * @return
+     * @throws IOException
+     */
+    public static boolean deleteFile(String fileName) throws IOException{
+        File file = new File(fileName);
+        if(!file.exists()){
+            return true;
+        }
+        if (file.isFile()){
+            return file.delete();
+        }else {
+            throw new IOException("File["+ file +"]is not a normal file");
         }
     }
 }
--- src/main/java/com/example/webtwo/demo/utils/StreamUtil.java
+++ src/main/java/com/example/webtwo/demo/utils/StreamUtil.java
@@ -19,13 +19,12 @@ public class StreamUtil {
     private ObjectMapper mapper = new ObjectMapper();
 
     /**
-     * 使用PrintWriter发送字符流
-     * @param dirUrl
-     * @param request
-     * @param response
+     * 使用PrintWriter发送字符流，已获取日志目录为例进行说明
+     * @param dirUrl  日志完整路径
+     * @param request HttpServletRequest
      * @return
      */
-    public String logDir(String dirUrl, HttpServletRequest request, HttpServletResponse response) {
+    public String logDir(String dirUrl, HttpServletRequest request) {
         logger.info("日志路径：" + dirUrl);
         String requestResult = "";
         PrintWriter requestOut = null;
@@ -51,14 +50,99 @@ public class StreamUtil {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 requestIn.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
                 requestOut.close();
-            }catch (IOException e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
         return requestResult;
+    }
+
+    /**
+     * 压缩并打包文件
+     * 将压缩包写入response输出流
+     * @param filePath 文件完整路径
+     */
+    public void filePackage(String filePath, HttpServletResponse response) {
+        logger.info("文件路径:[{}]", filePath);
+        byte[] appFileByte = null;
+        try {
+            //获取文件
+            File[] appFile = FileUtil.getAppZipFile(filePath);
+            if (appFile != null && appFile.length > 0) {
+                //设置临时路径
+                String tempFile = FileUtil.getTempPath(filePath, "_temp.zip");
+                //压缩文件
+                FileUtil.zipFiles(tempFile, appFile);
+                //读取临时文件
+                appFileByte = FileUtil.readBinaryDataFromFile(tempFile);
+                //删除临时文件
+                FileUtil.deleteFile(tempFile);
+            } else {
+                logger.error("不存在文件");
+            }
+            //返回数据
+            response.getOutputStream().write(appFileByte);
+            response.flushBuffer();
+            response.getOutputStream().close();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    /**
+     * 输出到浏览器下载
+     * @param fileName 文件名称
+     * @param fileStream 文件字节数组
+     * @param response httpServletResponse
+     */
+    public void explorerDownload(String fileName, byte[] fileStream, HttpServletResponse response) {
+        InputStream in = null;
+        OutputStream out = null;
+        if (fileStream == null || fileStream.length < 0) {
+            return;
+        }
+        //将字节转为字节流，便于输出到浏览器下载
+        in = new ByteArrayInputStream(fileStream);
+        //设置下载名字
+        response.addHeader("Content-Disposition", "attachment;fileName=" + fileName + ".zip");
+        //设置下载类型
+        response.setContentType("application/octet-stream");
+        try {
+            //输出流
+            out = new BufferedOutputStream(response.getOutputStream());
+            //写入输出流
+            byte[] buff = new byte[4096];
+            int i = 0;
+            while ((i = in.read(buff)) > 0){
+                out.write(buff,0,i);
+            }
+            logger.info("下载成功，文件名["+fileName+".zip]");
+        } catch (IOException e) {
+            e.printStackTrace();
+            logger.error(e.getMessage());
+        }finally {
+            try {
+                if(in != null){
+                    in.close();
+                }
+            }catch (Exception e){
+                logger.error(e.getMessage());
+            }
+            try {
+                if(out != null){
+                    out.close();
+                }
+            }catch (Exception e){
+                logger.error(e.getMessage());
+            }
+        }
     }
 }
